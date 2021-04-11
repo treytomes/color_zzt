8662787e3a1138b1f6ef3088c8fb190ff8c0b39a
Trey Tomes <trey.tomes@gmail.com>
2021-04-11 17:26:59 -0500

    Render the entire character set in all available colors.

--- demo/ascii.asm
+++ demo/ascii.asm
@@ -16,44 +16,39 @@ BytesPerChar	EQU	CharHeight
 ASCII:
 	includebin OEM437.bin
 
-ASCIIPointers:
-	;RMD			256
-	FDB ASCII+0
-	FDB ASCII+8
-	FDB ASCII+65*8
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	FCB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+TextBackground:
+		RMB		1										; Background color should have the same number in the high and low nibbles.
+TextForegroundHigh:
+		RMB		1										; Store the foreground color in the high nibble.
+TextForegroundLow:
+		RMB		1										; Store the foreground color in the low nibble.
 
 ;-------------------------------------------------------------------------------
-; Subroutine:	PopulateASCIIPointers
-; Summary:		Generate the pointer table at ASCIIPointers for accessing character data.
-; Remarks:		Clobbers X, Y, and B.
+; Subroutine:	SetColor
+; Summary:		Set the foreground and background text colors.
+; Parameters:
+;	B:	The color attribute.  High nibble is background, low nibble is foreground.
 ;-------------------------------------------------------------------------------
-PopulateASCIIPointers:
-		LDX		#ASCII									; Load the address of ASCII into X.
-		LDY		#ASCIIPointers							; Load the address of ASCIIPointers into Y.
-		CLRB
-	@Loop:
-		STX		,Y++									; Store the current address into ASCIIPointers, then move to the next pointer.
-		LEAX	BytesPerChar,X							; Move X to the next character.
-		;LEAY	2,Y
-		INCB
-		BNE		@Loop									; Keep looping until B overflows.
-		RTS
+SetColor:
+		PSHS	D
+	; Calculate the 3 color variables from B.
+		TFR		B,A										; A=B
+		ANDB	#$F0									; Isolate the background.
+		STB		TextBackground
+		LSRB
+		LSRB
+		LSRB
+		LSRB											; Move background into the low nibble.
+		ORB		TextBackground							; Recombine with background to duplicate the value.
+		STB		TextBackground							; This should save the final background value.
+		ANDA	#$0F									; Isolate the foreground.
+		STA		TextForegroundLow
+		LSLA
+		LSLA
+		LSLA
+		LSLA											; Move the attribute into the high nibble.
+		STA		TextForegroundHigh
+		PULS	D,PC
 
 ;-------------------------------------------------------------------------------
 ; Subroutine:	DrawChar
@@ -63,29 +58,19 @@ PopulateASCIIPointers:
 ;	X:	Row/column. (TODO)
 ;-------------------------------------------------------------------------------
 DrawChar:
-	; Calculate the 3 color variables from B.
-		PSHS	B										; Save the color for a moment.
-		ANDB	#$F0									; Isolate the background.
-		STB		@Background
-		LSRB
-		LSRB
-		LSRB
-		LSRB											; Move background into the low nibble.
-		ORB		@Background								; Recombine with background to duplicate the value.
-		STB		@Background								; This should save the final background value.
-		PULS	B										; Re-load the full color attribute.
-		ANDB	#$0F									; Isolate the foreground.
-		STB		@Foreground2
-		LSLB
-		LSLB
-		LSLB
-		LSLB											; Move the attribute into the high nibble.
-		STB		@Foreground1
-	; All done loading color variables.
-		LDX		#ScreenStart
-		LDY		#(ASCII+65*BytesPerChar)
-		;LDY		#ASCIIPointers
-		;LEAY	2,Y
+		PSHS	D,X,Y
+	; Begin calculating ASCII table pointer.
+		TFR		A,B										; B=A
+		CLRA
+		ASLB											; D*=8
+		ROLA
+		ASLB
+		ROLA
+		ASLB
+		ROLA
+		LDY		#ASCII
+		LEAY	D,Y
+	; Done calculating ASCII table pointer.
 		LDB		#CharHeight
 	@RowLoop:
 		PSHS	B
@@ -93,17 +78,17 @@ DrawChar:
 		LDA		,Y+
 	@ColLoop:
 		PSHS	B
-		LDB		@Background								; Load the background color.
+		LDB		TextBackground							; Load the background color.
 		TSTA
 		BPL		@Skip1									; Is the high bit 0?
 		ANDB	#$0F									; Clear out the background color.
-		ORB		@Foreground1							; Load the foreground color.
+		ORB		TextForegroundHigh						; Load the foreground color.
 	@Skip1:
 		LSLA
 		;TSTA											; Don't need to test A when we just operated on it.
 		BPL		@Skip2									; Is the high bit 0?
 		ANDB	#$F0									; Clear out the background color.
-		ORB		@Foreground2							; Load the foreground color.
+		ORB		TextForegroundLow						; Load the foreground color.
 	@Skip2:
 		LSLA
 		STB		,X+										; Write the next 2 pixels to the screen.
@@ -114,13 +99,7 @@ DrawChar:
 		LEAX	(ScreenByteWidth-BytesPerChar/PixelsPerByte),X
 		DECB
 		BNE		@RowLoop
-		RTS
-	@Background:
-		RMB		1										; Background color should have the same number in the high and low nibbles.
-	@Foreground1:
-		RMB		1										; Store the foreground color in the high nibble.
-	@Foreground2:
-		RMB		1										; Store the foreground color in the low nibble.
+		PULS	D,X,Y,PC
 
 Start:
 	; Disable interrupts.  Not sure yet why it's needed, but it is.
@@ -128,7 +107,10 @@ Start:
 	; This sets the stack pointer.  Probably only needed if your program start is inside the existing stack.
 		LDS		#$5FF
 
-		;JSR		PopulateASCIIPointers
+    ; High speed!
+		CLRA
+		STA		$FFD9
+		STA		$FFD7
 
 		JSR		EnableVSync	
 		JSR		SetGraphicsMode
@@ -137,9 +119,28 @@ Start:
 		LDX		#Palette
 		JSR		BuildPalette
 
-		LDA		#'*'									; The character to draw.
-		LDB		#$1F									; The color attribute.
+		LDB		#$0F									; The color attribute.
+	@ColorLoop:
+		JSR		SetColor
+		LDX		#ScreenStart
+		LDA		#0										; The character to draw.
+		LDY		#24										; 24 text rows.
+	@RenderRowsLoop:
+		PSHS	Y
+		LDY		#32										; 32 text columns.
+	@RenderLoop:
 		JSR		DrawChar
+		INCA
+		;ADDB	#11
+		LEAX	(CharWidth/PixelsPerByte),X
+		LEAY	-1,Y
+		BNE		@RenderLoop
+		PULS	Y
+		LEAX	((CharHeight-1)*ScreenByteWidth),X
+		LEAY	-1,Y
+		BNE		@RenderRowsLoop
+		ADDB	#$11
+		BRA		@ColorLoop
 		
 Loop:
 		BRA	Loop
